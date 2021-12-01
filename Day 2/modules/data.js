d6b71c904e14423fb294f1502bdacaa871d2f8ad
Saroj Dahal <sarojdahal1213@gmail.com>
2021-12-01 19:48:22 +0545

    added: Day 2 dir

---
+++ Day 2/modules/data.js
@@ -0,0 +1,13 @@
+export function add(a, b) {
+  console.log(user);
+  return a + b;
+}
+
+export var user = {
+  id: 1,
+  name: "Pratik",
+};
+
+// export {add};
+
+// add()
---
+++ Day 2/modules/main.js
@@ -0,0 +1,4 @@
+import {add as myAdder, user as userObject} from "./data.js";
+
+// console.log(add(1, 2));
+console.log(userObject);
