766021f722f134d408e998782d04e39b781717bd
Saroj Dahal <sarojdahal1213@gmail.com>
2021-12-03 19:50:00 +0545

    added: Day 4 dir

---
+++ Day 4/sort.js
@@ -0,0 +1,13 @@
+const numbers = [23, 123, 1];
+console.log(numbers.sort());
+numbers.sort(function () {});
+
+//function overloading
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+//function overriding.
